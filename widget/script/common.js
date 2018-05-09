ddf010dc40ab7d35c298450a575b3f660c1120f3
Qi, Ryan (BJG-TIG) <Ryan.Qi@ipgteamignite.com>
2018-05-09 14:48:20 +0800

    添加底部菜单

--- widget/script/common.js
+++ widget/script/common.js
@@ -7,3 +7,32 @@ function fnStart() {
         name: 'welcome_group'
     });
 };
+
+// open overlay
+function openCreateOverlay(){
+    var frames = [];
+    frames.push({
+        name: 'overlay-create',
+        url: './html/overlay-create.html',
+        bounces: false,
+    });
+    api.openFrameGroup({
+        name: 'overlay-create',
+        scrollEnabled: true,
+        rect: {
+            x: 0,
+            y: 0,
+            w: 'auto',
+            h: 'auto'
+        },
+        index: 1,
+        frames: frames
+    });
+};
+
+// close overlay
+function closeCreateOverlay(){
+    api.closeFrameGroup({
+        name: 'overlay-create'
+    });
+};
